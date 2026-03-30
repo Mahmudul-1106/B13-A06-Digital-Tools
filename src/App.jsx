142f843e7abad92a0ac3cb3a3e6cbd4a326216ca
mahmudul <mahmudul1106@gmail.com>
2026-03-30 06:10:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-amber-400 font-extrabold text-white">Welcome</h1>
+      <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
